5df390e4a3dd3f092bdc0d6cc6883528e8b1fb6c
LEEJAEBIN <dmlwlsdk07@gmail.com>
2017-11-17 23:54:35 +0900

    add show_comment #24 #31

---
+++ Application/Server/show_comment.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var mysql = require('mysql');
+
+app.use(express.static('../Client'));
+app.use(bodyParser.urlencoded({ extended : false}));
+
+exports.comment = function(req, res){
+    var conn = mysql.createConnection({
+        host : "localhost",
+        user : "root",
+        password : "12341234",
+        database : "tipper"
+    });
+    var post_id = req.body.post_id;
+    var type = req.body.type;
+    var result = "";
+    if(type==='t'){
+        var sql = "select * from `t_comment` WHERE post_id = '"+post_id+"'";
+        console.log(sql);
+        conn.query(sql, function(err, results){
+            var length = results.length;
+            if(err){
+                res.json({status : 'f'});
+                throw err;
+            }
+            for(var i=0;i<length;i++){
+                result = result+ '{comment : '+results[i].comment+', writer : '+results[i].account_id+', createTime : '+results[i].createTime+'}';
+            }
+            res.json({results : result});
+        });
+    }
+    else if(type==='f'){
+        var sql = "select * from `f_comment` WHERE post_id = '"+post_id+"'";
+        console.log(sql);
+        conn.query(sql, function(err, results){
+            var length = results.length;
+            if(err){
+                res.json({status : 'f'});
+                throw err;
+            }
+            for(var i=0;i<length;i++){
+                result = result+ '{comment : '+results[i].comment+', writer : '+results[i].account_id+', createTime : '+results[i].createTime+'}';
+            }
+            res.json({results : result});
+        });
+    }
+}
