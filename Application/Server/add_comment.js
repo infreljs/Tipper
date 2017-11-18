0330570a52100c9111d075c3b20bf32531fa4c56
LEEJAEBIN <dmlwlsdk07@gmail.com>
2017-11-18 21:36:44 +0900

    update show_comment, add add_comment #31 #24

--- Application/Server/add_comment.js
+++ Application/Server/add_comment.js
@@ -2,11 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var mysql = require('mysql');
+require('date-utils');
+
 
 app.use(express.static('../Client'));
 app.use(bodyParser.urlencoded({ extended : false}));
 
-exports.comment = function(req, res){
+exports.add_comment = function(req, res){
     var conn = mysql.createConnection({
         host : "localhost",
         user : "root",
@@ -15,24 +17,26 @@ exports.comment = function(req, res){
     });
     var post_id = req.body.post_id;
     var type = req.body.type;
+    var comment = req.body.comment;
+    var user_id = req.body.user_id;
     var result = "";
+    var dt = new Date();
+    var d = dt.toFormat('YYYY-MM-DD HH24:MI:SS');
     if(type==='t'){
-        var sql = "select * from `t_comment` WHERE post_id = '"+post_id+"'";
+        var sql = "insert into t_comment (post_id, comment, account_id, createTime) VALUES('"+post_id+"', '"+comment+"', '"+user_id+"', '"+d+"')";
         console.log(sql);
-        conn.query(sql, function(err, results){
-            var length = results.length;
+        conn.query(sql, function(err, result){
             if(err){
                 res.json({status : 'f'});
                 throw err;
             }
-            for(var i=0;i<length;i++){  
-                result = result+ '{comment : '+results[i].comment+', writer : '+results[i].account_id+', createTime : '+results[i].createTime+'}';
+            if(result.affectedRows==1){
+                res.json({status : 's'});
             }
-            res.json({results : result});
         });
     }
     else if(type==='f'){
-        var sql = "select * from `f_comment` WHERE post_id = '"+post_id+"'";
+        var sql = "insert into f_comment (post_id, comment, account_id, createTime) VALUES('"+post_id+"', '"+comment+"', '"+user_id+"', '"+d+"')";
         console.log(sql);
         conn.query(sql, function(err, results){
             var length = results.length;
