2c1eea68dbc13084b0f9c31aee0cdcd45ac85287
LEEJAEBIN <dmlwlsdk07@gmail.com>
2017-11-20 19:02:27 +0900

    Fixed payed_tips.js show_comment.js #32 #31 #24

--- Application/Server/show_comment.js
+++ Application/Server/show_comment.js
@@ -15,7 +15,7 @@ exports.comment = function(req, res){
     });
     var post_id = req.body.post_id;
     var type = req.body.type;
-    var result = "";
+    var result = [];
     if(type==='t'){
         var sql = "select * from `t_comment` WHERE post_id = '"+post_id+"'";
         console.log(sql);
@@ -26,9 +26,11 @@ exports.comment = function(req, res){
                 throw err;
             }
             for(var i=0;i<length;i++){  
-                result = result+ '{comment : '+results[i].comment+', writer : '+results[i].account_id+', createTime : '+results[i].createTime+'}';
+                //result = result+ '{comment : '+results[i].comment+', writer : '+results[i].account_id+', createTime : '+results[i].createTime+'}, ';
+                result.push({comment : results[i].comment, writer : results[i].account_id, createTime : results[i].createTime});
             }
-            res.json({results : result});
+            res.contentType('application/json');
+            res.send(JSON.stringify(result));
         });
     }
     else if(type==='f'){
@@ -41,9 +43,10 @@ exports.comment = function(req, res){
                 throw err;
             }
             for(var i=0;i<length;i++){
-                result = result+ '{comment : '+results[i].comment+', writer : '+results[i].account_id+', createTime : '+results[i].createTime+'}';
+                result.push({comment : results[i].comment, writer : results[i].account_id, createTime : results[i].createTime});
             }
-            res.json({results : result});
+            res.contentType('application/json');
+            res.send(JSON.stringify(result));
         });
     }
 }
