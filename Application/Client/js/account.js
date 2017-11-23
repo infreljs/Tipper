07ad20a97abb9a8ccc933cfc07e269d108dd97b6
Lee Jaeseok <infreljs@gmail.com>
2017-11-23 09:45:00 +0900

    Update Client

---
+++ Application/Client/js/account.js
@@ -0,0 +1,60 @@
+function login() {
+    var id = $('#id').val();
+    var pw = $('#pw').val();
+
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: {
+            id: id,
+            pw: pw
+        },
+        dataType: "json",
+        success: function (data, status, xhr) {
+
+        },
+        error: function (xhr, status, error) {
+
+        }
+    });
+}
+
+function logout() {
+
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: {
+            id: id,
+            pw: pw
+        },
+        dataType: "json",
+        success: function (data, status, xhr) {
+
+        },
+        error: function (xhr, status, error) {
+
+        }
+    });
+}
+
+function register() {
+    var id = $('#id').val();
+    var pw = $('#pw').val();
+
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: {
+            id: id,
+            pw: pw
+        },
+        dataType: "json",
+        success: function (data, status, xhr) {
+
+        },
+        error: function (xhr, status, error) {
+
+        }
+    });
+}
